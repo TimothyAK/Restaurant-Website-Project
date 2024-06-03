fceaf856ae6b5ba32c7bbedbfbe6002bf479116d
TimothyAK <mothyabraham973@gmail.com>
2024-06-03 10:24:39 +0700

    organize Files

---
+++ pages/about/about.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ABOUT | SABORES DEL SUR</title>
+
+    <!-- Bootstrap Icon-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <!-- Merriweather Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+
+    <!-- Farro -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Farro:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+    <!-- CSS Stylesheet Links -->
+    <link rel="stylesheet" href="style1.css">
+
+
+    <!-- JQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+    <!-- Navbar -->
+    <div id="navbar" class="container">
+        <div class="container">
+            <ul class="navbar-list">
+                <li class="navbar-item">
+                    <a href="/serverSIdeWebCourse/RestaurantWebProject/pages/home/home.html">HOME</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="Menu.pdf" target="_blank">MENU</a>
+                </li>
+                <li class="navbar-item">
+                    <img id="navbar-brand" src="images/restaurant_logo.png" alt="">
+                </li>
+                <li class="navbar-item">
+                    <a href="#">ABOUT</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="#">RESERVE</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+        
+        <!--Image-->
+        <div class = "parallax">
+        </div>
+
+        <!--Story-->
+        <div id="about-story">
+            <br><br>
+            <hr class="left-hr">
+            <hr class="right-hr">
+            <h2>Sabores del Sur is a casual and intimate Spanish-inspired restaurant, specializing in fresh, handmade pasta and family-style dishes designed to share.</h2>
+            <p>With the warm hospitality and energy of a traditional Italian home, we offer family-style service: all dishes are designed for sharing and the food arrives in the middle of the table, as it is prepared. The antipasti arrives first, with pasta courses close behind. We also offer house-made gluten-free options available daily. All menu items are available for dine-in or take-out.</p>
+            <hr class="left-hr">
+            <hr class="right-hr">
+        </div>
+
+        <br><br>
+
+        <!--Food-->
+        <div id="about-food">
+            <center><br><br><br>
+            <table width="75%">
+            <tr>
+                <td width="50%"><h3>Tarta de Santiago</h3>
+                <p>Each decadent slice is a celebration of simplicity and elegance, paying homage to centuries-old recipes passed 
+                down through generations. Indulge in a slice of history and savor the timeless allure of Tarta de Santiago, a 
+                culinary masterpiece that transcends time and borders.
+                </p>
+            </td>
+                <td><center><img class = "kotak4" src="images/santiago.jpg" ></center></td>
+            </tr>
+            </table>    
+            </center>
+        </div>
+
+        <div id="about-food">
+            <center>
+            <table width="75%">
+            <tr>
+                <td><center><img class = "kotak3" src="images/sangria.jpg" ></center>
+            </td>
+                <td width="50%"><h3>Sangria</h3>
+                <p>Crafted by blending robust red wine with a medley of luscious fruits such as oranges, lemons, and berries, 
+                    this refreshing concoction boasts a tantalizing harmony of sweet and tangy flavors. Infused with a hint of 
+                    citrusy zest and the subtle richness of brandy, each glass of Red Sangria is a sensory journey, evoking images 
+                    of sun-kissed vineyards and lively fiestas. Whether enjoyed leisurely on a sunny terrace or served alongside a 
+                    spread of tapas, this iconic Spanish drink captures the convivial spirit of gatherings and celebrates the joy of 
+                    shared moments
+                </p>
+                </td>
+            </tr>
+            </table>    
+            </center>
+        </div>
+
+        <!--More Food-->
+        <div id="about-end">
+            <h3>Paela Mixta</h3>
+            <p>The rice, cooked to a delectable golden hue, absorbs the rich flavors of the ingredients, while aromatic spices 
+                such as paprika and garlic add depth to every mouthful. Served in a traditional shallow pan, Paella Mixta is a 
+                feast for the senses, embodying the spirit of Spanish gastronomy and bringing people together in celebration of 
+                good food and camaraderie.
+            </p>
+        </div>
+        <div id="about-end">
+            <h3>Tortilla Espanola</h3>
+            <p>Combination of thinly sliced potatoes, farm-fresh eggs, and savory onions, delicately seasoned and lovingly crafted 
+                into a golden, fluffy omelette. Perfect for any time of day, this iconic Spanish favorite is sure to transport your 
+                taste
+            </p>
+            <br><br><br>
+        </div>
+
+        <!--Image-->
+        <div class = "parallax1">
+        </div>
+
+        <!--Footer-->
+        <div id="footer" class="container">
+        <ul>
+            <li>
+                <a href="#navbar">Home</a>
+            </li>
+            <li>
+                <a id="menu-link" href="Menu.pdf" target="_blank">Menu</a>
+            </li>
+            <li>
+                <a id="about-link" href="">About</a>
+            </li>
+            <li>
+                <a id="reserve-link" href="">Reserve</a>
+            </li>
+        </ul>
+        <h1>Sabores del Sur</h1>
+    </div>  
+    
+    <script src="about.js" type="text/javascript"></script>
+    
+</body>
+</html>
---
+++ pages/home/home.js
@@ -0,0 +1,49 @@
+$(document).ready(setInitialNavbarMargin);
+$(document).ready(underlineCurrentPage);
+
+window.addEventListener("resize", updateNavbarMargin, true);
+
+document.querySelector("#scrollDownButton").addEventListener("click", scrollDown);
+
+let initialScreenWidth = 0;
+let initialNavbarListLeftOffset = 0;
+
+function setInitialNavbarMargin() {
+    let navbarBrand = document.getElementById("navbar-brand");
+    let navbarList = document.querySelectorAll(".navbar-list")[0];
+
+    initialScreenWidth = window.innerWidth;
+    let navbarBrandWidth = navbarBrand.getBoundingClientRect().width;
+
+    let navbarBrandLeftCoordinate = navbarBrand.offsetLeft;
+    
+    initialNavbarListLeftOffset = (initialScreenWidth / 2) - navbarBrandLeftCoordinate - (navbarBrandWidth / 2);
+
+    console.log("B");
+
+    navbarList.style["left"] = initialNavbarListLeftOffset + "px";
+}
+
+function updateNavbarMargin() {
+    let navbarList = document.querySelectorAll(".navbar-list")[0];
+
+    let currentScreenWidth = window.innerWidth;
+    let currentNavbarListLeftOffset;
+
+    currentNavbarListLeftOffset = initialNavbarListLeftOffset - ((initialScreenWidth - currentScreenWidth) / 2);
+
+    console.log("A");
+    
+
+    navbarList.style["left"] = currentNavbarListLeftOffset + "px";
+}
+
+function scrollDown() {
+    window.scrollTo(0, 691);
+}
+
+function underlineCurrentPage() {
+    if(document.URL.includes("home.html")) {
+        document.querySelector("a#home-link").classList.add("active");
+    }
+}
