2945a372024c871ee927a1ffb3df33f0b8476e5f
davidl1459 <davidryanlie01@gmail.com>
2024-06-14 18:13:45 +0700

    add Reserve page

---
+++ pages/Reserve/reserve.js
@@ -0,0 +1,52 @@
+$(document).ready(setInitialNavbarMargin);
+$(document).ready(underlineCurrentPage);
+
+window.addEventListener("resize", updateNavbarMargin, true);
+
+let initialScreenWidth = 0;
+let initialNavbarListLeftOffset = 0;
+
+function setInitialNavbarMargin(){
+    let navbarBrand = document.getElementById("navbar-brand");
+    let navbarList = document.querySelectorAll(".navbar-list")[0];
+
+    initialScreenWidth = window.innerWidth;
+    let navbarBrandWidth = navbarBrand.getBoundingClientRect().width;
+
+    let navbarBrandLeftCoordinate = navbarBrand.offsetLeft;
+
+    initialNavbarListLeftOffset = (initialScreenWidth / 2) - navbarBrandLeftCoordinate - (navbarBrandWidth / 2);
+
+    console.log("B");
+
+    navbarList.style["left"] = initialNavbarListLeftOffset + "px";
+}
+
+function updateNavbarMargin() {
+    let navbarList = document.querySelectorAll(".navbar-list")[0];
+
+    let currentScreenWidth = window.innerWidth;
+    let currentNavbarListLeftOffset;
+
+    currentNavbarListLeftOffset = initialNavbarListLeftOffset - ((initialScreenWidth - currentScreenWidth) / 2);
+
+    console.log("A");
+
+
+    navbarList.style["left"] = currentNavbarListLeftOffset + "px";
+}
+
+function underlineCurrentPage() {
+    if(document.URL.includes("home.html")) {
+        document.querySelector("a#home-link").classList.add("active");
+    } else if(document.URL.includes("about.html")) {
+        document.querySelector("a#about-link").classList.add("active");
+    } else if(document.URL.includes("reserve.html")) {
+        document.querySelector("a#reserve-link").classList.add("active");
+    }
+}
+
+function redirectToConfirmation(event){
+    event.preventDefault();
+    window.location.href = '/finalproject/Reserve/submit-reservation.html';
+}
